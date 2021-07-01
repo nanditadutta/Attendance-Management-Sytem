f5a5ddeed2b716a7d7ec6ce37d4fca6c6510a822
nanditadutta <46259506+nanditadutta@users.noreply.github.com>
2021-07-01 14:24:49 +0545

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -20,19 +20,9 @@ firebase.database().ref('attendance-management-sy-59713-default-rtdb').on('value
     // })
 
     Object.values(recieveData).forEach((entry, index)=>{
-        res+="<div class='card'>";
-        res+="<div class='card-header' id='"+Object.keys(recieveData)[cnt]+"'>";
-        res+="<h5 class='mb-0'>";
-        res+=" <button class='btn ' data-toggle='collapse' data-target='#"+Object.keys(recieveData)[cnt]+"' aria-expanded='false' aria-controls='"+Object.keys(recieveData)[cnt]+"'>";
         res+=Object.keys(recieveData)[cnt];
-        res+="    </button>";
-        res+="  </h5>";
-        res+=" </div>";
-    
-        res+="<div id='"+Object.keys(recieveData)[cnt]+"' class='collapse show' aria-labelledby='"+Object.keys(recieveData)[cnt]+"' data-parent='#accordion'>";
-        res+="<div class='card-body'>";
-        res+="<table class='table  mx-5 mb-5 mt-2 table-bordered w-75'>";
-        res+="<thead><tr class='table-primary'><td>Date</td><td>Enrollment Id</td><td>Name</td></tr></thead><tbody class='table table-dark table-hover'>";
+        res+="<table class='table table-bordered table-hover w-50  align-middle'>";
+        res+="<thead class='table-primary table-dark'><tr class=''><td>Date</td><td>Enrollment Id</td><td>Name</td></tr></thead><tbody class='table-sm'>";
         Object.values(entry).forEach((data, index)=>{
                 res+="<tr>";
                 res+="<td>"+data["Date"]+"</td>";
